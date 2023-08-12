5d511b4f7d5af79fd218c0f3b72b8db71dfd800b
Wrexan <fwrexan@gmail.com>
2023-08-12 20:56:37 +0300

    moved final price recalculation to task

--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch, F, Sum
+from django.db.models import Prefetch, Sum
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import Client
@@ -10,15 +10,13 @@ class SubscriptionView(ReadOnlyModelViewSet):
     # queryset = Subscription.objects.all().prefetch_related('client').prefetch_related('client__user')
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
-        'service',
         Prefetch(
             'client', queryset=Client.objects.all().select_related('user').only(
                 'company_name',
                 'user__email'
             )
         )
-    ).annotate(price=F('service__full_price') -
-                     F('service__full_price') * F('plan__discount_percent') / 100.00)
+    )
     serializer_class = SubscriptionSerializer
 
     # add aggregated summ and put query list in new parameter 'result'
